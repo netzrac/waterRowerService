e4aeddc6f2f7b1f06b75e5a1841976d42038c557
cp <cp.eclipsing@googlemail.com>
2019-07-01 20:27:05 +0200

    init

---
+++ src/waterRowerService/DataConnectorException.java
@@ -0,0 +1,10 @@
+package waterRowerService;
+
+public class DataConnectorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7783833912822691718L;
+
+}
---
+++ test/waterRowerService/DataRecordTest.java
@@ -0,0 +1,19 @@
+package waterRowerService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataRecordTest {
+
+	@Test
+	public void testToString() throws DataRecordException {
+		DataRecord dataRec=new DataRecord("A8000310008010311021050047302");
+		String toString=dataRec.toString();
+		String expectedString=new String("31 80\n");
+		assertTrue(expectedString.equals( toString));
+		dataRec=new DataRecord("A8001310008010311021050047302");
+		assertTrue(new String( "91 80\n").equals(dataRec.toString()));
+	}
+
+}
