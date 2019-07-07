9c2d3755dbf4c4ce8b1110aafb1e1698b51348e8
cp <cp.eclipsing@googlemail.com>
2019-07-07 10:03:36 +0200

    sleep added between cmds

--- src/waterRowerService/WaterRowerService.java
+++ src/waterRowerService/WaterRowerService.java
@@ -22,22 +22,40 @@ public class WaterRowerService {
 		DataNotifier notifier=new SimpleDataNotifier();
 
 		SerialDataConnector sdc=new SerialDataConnector();
+		int waitfor=200;
 		sdc.register(notifier);
 		sdc.write( "C\n");
+		sleep(waitfor);
 		sdc.write( "T\n");
+		sleep(waitfor);
 		sdc.write( "V\n");
+		sleep(waitfor);
 		sdc.write( "L\n");
+		sleep(waitfor);
 		sdc.write( "H\n");
+		sleep(waitfor);
 		sdc.write( "R\n");
 		
+		sleep();
+
+	}
+
+	private static void sleep() {
+		sleep(0);
+	}
+	
+	private static void sleep(int i) {
 		try {
 			synchronized (semaphore) {
-				semaphore.wait();
+				if( i==0) {
+					semaphore.wait();
+				} else {
+					semaphore.wait(i);
+				}
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
